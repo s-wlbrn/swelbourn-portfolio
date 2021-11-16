045c4ab0aa262f1c62bf98edc3408ab1dca35145
Stephen Welbourn <welbourn.stephen@gmail.com>
2021-11-16 04:23:43 -0600

    remove useless font and css imports, switch montserrat to fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
 // custom typefaces
-import "typeface-montserrat"
+import "@fontsource/montserrat/700.css"
 import "@fontsource/work-sans/500.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
