07a6d612d026045bdf64f3815e1f5a6e0019172f
Stephen Welbourn <welbourn.stephen@gmail.com>
2021-11-16 03:43:34 -0600

    roll back to gatsby-plugin-loadable-components-ssr 4.0 0

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     },
   },
   plugins: [
-    "gatsby-plugin-loadable-components-ssr",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -140,5 +139,6 @@ module.exports = {
     `gatsby-plugin-react-svg`,
     `gatsby-plugin-perf-budgets`,
     `gatsby-plugin-webpack-bundle-analyser-v2`,
+    "gatsby-plugin-loadable-components-ssr",
   ],
 }
