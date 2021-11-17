fe001d0f8e5851ea2506b30200ad4883e8bf1e1f
Stephen Welbourn <welbourn.stephen@gmail.com>
2021-11-17 07:34:43 -0600

    edit "about" text

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -9,11 +9,11 @@ export const About = () => {
         <h2>Hi, I'm Stephen.</h2>
         <p>
           I'm a full stack web developer with a focus on front-end development,
-          currently based in Nashville, TN. As a developer and a musician, I
-          have a great appreciation for working as a team with talented people.
-          I strive to use modular and performant code to create apps that make
-          people happy, while maintaining a mindset of curoiosity and continual
-          growth.
+          currently based in Nashville, TN. As both a developer and a musician,
+          I have a great appreciation for working as a team with talented
+          people. I strive to use modular and performant code to create apps
+          that make people happy while maintaining a mindset of curiosity and
+          continual growth.
         </p>
       </section>
     </div>
