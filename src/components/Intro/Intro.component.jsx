88d4effa99fc571fc019d1dc6d1d3feb7cd17bb4
Stephen Welbourn <welbourn.stephen@gmail.com>
2021-11-17 06:12:23 -0600

    remove particles

--- src/components/Intro/Intro.component.jsx
+++ src/components/Intro/Intro.component.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import Particles from 'react-tsparticles';
-import { particlesOptions } from './particlesOptions';
 
 import './Intro.styles.scss';
 
 export const Intro = () => {
-  const particlesLoaded = (container) => {
-    container.canvas.element.style.opacity = 1;
-  };
-
   return (
     <header className="intro">
-      <Particles
-        loaded={particlesLoaded}
-        id="tsparticles"
-        style={{ position: 'absolute', left: 0, top: 0, zIndex: 0 }}
-        canvasClassName="intro-particles"
-        options={particlesOptions}
-      />
-
       <h1>
         Stephen
         <br />
