66a8bd4d7f5b5271432f221c8d175573400441b7
Stephen Welbourn <welbourn.stephen@gmail.com>
2022-04-18 16:35:29 -0500

    use custom vh variable to fix banner height on mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,11 @@ import { Skills } from '../components/Skills/Skills.component';
 import { Projects } from '../components/Projects/Projects.component';
 import { Contact } from '../components/Contact/Contact.component';
 
+// get the viewport height and derive real value for a vh unit
+const vh = window.innerHeight * 0.01;
+// set the value in the --vh custom property to root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const Homepage = () => {
   return (
     <>
